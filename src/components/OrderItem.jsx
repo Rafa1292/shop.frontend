599725058fe7f769042537c8ab4fb9f4d39ab4fb
Rafael Villalobos <rvilla3452@gmail.com>
2022-07-20 01:08:49 -0600

    sizeOrderItem

--- src/components/OrderItem.jsx
+++ src/components/OrderItem.jsx
@@ -1,26 +1,52 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import AppContext from '../context/AppContext';
 import '@styles/OrderItem.scss';
 import close from '@icons/icon_close.png'
 import { formatMoney } from '@helpers/formatHelper'
+import { useGetList } from '../hooks/useAPI';
 
 const OrderItem = ({ item }) => {
 	const { removeFromCart } = useContext(AppContext);
+	const [sizes, setSizes] = useState([]);
 
 	const handleRemove = item => {
 		removeFromCart(item);
 	}
 
+	const loadSizes = async () => {
+        const response = await useGetList('sizes');
+        setSizes(response.data);
+    }
+
+	useEffect(async () => {
+        await loadSizes();
+    }, []);
+
+	const setSize = async (sizeId) => {
+		console.log(item)
+		item.productMove.sizeId = sizeId;
+	}
+
 	return (
 		<div className="OrderItem items-center d-flex">
-			<div className="col-2 center">
-			<img src={close} alt="close" height={15} width={15} onClick={() => handleRemove(item)} />
-			</div>
-			<figure className='col-3 center'>
-				<img src={item.productMove.product.image}/>
+			<strong className='center col-1'>{item.productMove.quantity}</strong>
+			<figure className='col-2 center'>
+				<img src={item.productMove.product.image} />
 			</figure>
-			<strong className='center col-2'>{item.productMove.quantity}</strong>
-			<strong className='col-3'>{formatMoney(item.price * item.productMove.quantity)}</strong>
+			<div className="col-3 center">
+				<select style={{ height: '40px' }} className='input col-8 p-2' onChange={(event) => setSize(event.target.value)}>
+					<option key={0} value="0">Talla</option>
+					{
+						sizes.map(size => (
+							<option key={size.id} value={size.id}>{size.name}</option>
+						))
+					}
+				</select>
+			</div>
+			<strong className='col-2 center'>{formatMoney(item.price * item.productMove.quantity)}</strong>
+			<div className="col-2 center">
+				<img src={close} alt="close" height={15} width={15} onClick={() => handleRemove(item)} />
+			</div>
 		</div>
 	);
 }
