be7386cece1b728a3d7c2fa8460e39afd2affbb5
Rafael Villalobos <rvilla3452@gmail.com>
2022-08-05 00:47:18 -0600

    workflow

--- src/pages/SubCategories.jsx
+++ src/pages/SubCategories.jsx
@@ -1,7 +1,7 @@
 import { useGetList, usePost, useDelete, usePatch } from '../hooks/useAPI';
 import React, { useRef, useState, useEffect } from "react";
 import Title from '@components/Title';
-import '@styles/subcategories.scss';
+import '@styles/subcategories';
 
 const SubCategories = () => {
     let [subcategories, setSubcategories] = useState([]);
